a29bb2381a843be549c498a001f80fd3c114223d
coriocactus <69796618+coriocactus@users.noreply.github.com>
2026-04-06 17:05:28 +0100

    mysql family

--- tests/settings.py
+++ tests/settings.py
@@ -1,8 +1,18 @@
 import os
 
-DB_BACKEND = os.environ.get("DB_BACKEND", "sqlite")
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+USE_TZ = True
 
-if DB_BACKEND in ("mysql", "mariadb"):
+DB_BACKEND = os.environ.get("DB_BACKEND", "sqlite")
+MYSQL_FAMILY = {"mysql", "mariadb"}
+
+DEFAULT_DB_USER = "dj_queue"
+DEFAULT_DB_PASSWORD = "dj_queue"
+if DB_BACKEND in MYSQL_FAMILY:
+  DEFAULT_DB_USER = "root"
+  DEFAULT_DB_PASSWORD = "root"
+
+if DB_BACKEND in MYSQL_FAMILY:
   import pymysql
 
   pymysql.version_info = (2, 2, 1, "final", 0)
@@ -17,35 +27,31 @@ _DATABASES = {
   "postgres": {
     "ENGINE": "django.db.backends.postgresql",
     "NAME": os.environ.get("DB_NAME", "dj_queue_test"),
-    "USER": os.environ.get("DB_USER", "dj_queue"),
-    "PASSWORD": os.environ.get("DB_PASSWORD", "dj_queue"),
+    "USER": os.environ.get("DB_USER", DEFAULT_DB_USER),
+    "PASSWORD": os.environ.get("DB_PASSWORD", DEFAULT_DB_PASSWORD),
     "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
     "PORT": os.environ.get("DB_PORT", "17432"),
   },
   "mysql": {
     "ENGINE": "django.db.backends.mysql",
     "NAME": os.environ.get("DB_NAME", "dj_queue_test"),
-    "USER": os.environ.get("DB_USER", "dj_queue"),
-    "PASSWORD": os.environ.get("DB_PASSWORD", "dj_queue"),
+    "USER": os.environ.get("DB_USER", DEFAULT_DB_USER),
+    "PASSWORD": os.environ.get("DB_PASSWORD", DEFAULT_DB_PASSWORD),
     "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
     "PORT": os.environ.get("DB_PORT", "17312"),
   },
   "mariadb": {
     "ENGINE": "django.db.backends.mysql",
     "NAME": os.environ.get("DB_NAME", "dj_queue_test"),
-    "USER": os.environ.get("DB_USER", "dj_queue"),
-    "PASSWORD": os.environ.get("DB_PASSWORD", "dj_queue"),
+    "USER": os.environ.get("DB_USER", DEFAULT_DB_USER),
+    "PASSWORD": os.environ.get("DB_PASSWORD", DEFAULT_DB_PASSWORD),
     "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
     "PORT": os.environ.get("DB_PORT", "17306"),
   },
 }
 
 DATABASES = {"default": _DATABASES[DB_BACKEND]}
-
-INSTALLED_APPS = [
-  "dj_queue",
-]
-
+INSTALLED_APPS = ["dj_queue"]
 DATABASE_ROUTERS = ["dj_queue.routers.DjQueueRouter"]
 
 TASKS = {
@@ -55,7 +61,3 @@ TASKS = {
     "OPTIONS": {},
   },
 }
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-USE_TZ = True
